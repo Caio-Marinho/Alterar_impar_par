f5d3fe9730e60a0d7fa5ad3a2a280c80c340aa73
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-23 09:22:51 -0300

    calculo matematico de acordo com valor de x

--- src/Impar_Par/alterar_impar_par.java
+++ src/Impar_Par/alterar_impar_par.java
@@ -1,5 +1,18 @@
 package Impar_Par;
+import java.util.Scanner;
 
 public class alterar_impar_par {
-
+	public static void main(String[] args) {
+		int x;
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Informe o valor de X: ");
+		x = entrada.nextInt();
+		entrada.close();
+		if (x % 2 == 0) {
+			x /= 2;
+		} else {
+			x = 3 * x + 1;
+		}
+		System.out.printf("O valor de X agora é : %d",x);
+	}
 }
